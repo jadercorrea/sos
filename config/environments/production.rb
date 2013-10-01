8a63c7b1cbee7aea542bd67548014a1dd68556e5
Luan Jodar <luan.jodar@gmail.com>
2013-10-01 20:21:39 -0300

    Compiling assets in production because of Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ ServiceOrders::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true # true because of heroku
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -17,7 +17,7 @@ ServiceOrders::Application.configure do
   config.eager_load = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # true because of heroku
 
   # Generate digests for assets URLs
   config.assets.digest = true
