bded64d607ab7671596f7517bb8531ffbcdcb226
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-10-22 22:10:38 -0200

    Adding messages to tickets

--- spec/acceptance/tickets_acceptance_spec.rb
+++ spec/acceptance/tickets_acceptance_spec.rb
@@ -5,15 +5,10 @@ feature "Tickets" do
   background do
     login_into_admin
     @client = FactoryGirl.create(:client, name: "Luan")
-    FactoryGirl.create(:ticket, client: @client)
+    @ticket = FactoryGirl.create(:ticket, client: @client)
   end
 
   scenario "As admin, I want to see the all the tickets" do
-    click_link "Tickets"
-    page.should have_content "Luan"
-  end
-
-  scenario "As admin, I want to define ticket's status" do
     click_link "Tickets"
     page.should have_content "Luan"
     page.should have_content "Pendente"
@@ -26,11 +21,42 @@ feature "Tickets" do
     select "Luan", from: "Cliente"
     page.should have_select "Status", options: ["Pendente", "Em andamento", "Resolvido"]
     select "Pendente", from: "Status"
-    fill_in "ticket_messages_attributes_0_text", with: "Aqui está um ticket"
-    click_button "Create ticket"
-    current_path.should == ticket_path(Ticket.last)
+    fill_in "message_field", with: "Aqui está um ticket"
+    click_button "Salvar"
+    current_path.should == tickets_path
+    page.should have_content "Luan"
+    page.should have_content "Pendente"
+
+    visit edit_ticket_path(Ticket.last)
     page.should have_content "Aqui está um ticket"
     page.should have_content "Luan"
     page.should have_content "Pendente"
+  end
+
+  describe "editing" do
+    background do
+      visit edit_ticket_path(@ticket)
+    end
+
+    scenario "As admin, I want to edit ticket's status" do
+      page.should have_content "Luan"
+      page.should have_content "Pendente"
+
+      select "Em andamento", from: "Status"
+      click_button "Salvar"
+      page.should have_content "Luan"
+      page.should have_content "Em andamento"
+    end
+
+    scenario "As admin, I want to add messages to a ticket" do
+      page.should have_content "This is a ticket message"
+
+      fill_in "message_field", with: "My second message"
+      click_button "Salvar"
+
+      page.should have_content "This is a ticket message"
+      page.should have_content "My second message"
+      current_path.should == edit_ticket_path(@ticket)
+    end
   end
 end
